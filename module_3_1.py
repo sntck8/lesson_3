502bca7dd05289b75189538b81eb630b4119e02b
sntck8 <sntck88@gmail.com>
2024-10-29 19:55:31 +0700

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,36 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls = calls + 1
+
+def string_info(string):
+    str_ = str(string)
+    result = ((len(str_)), str_.lower(), str_.upper())
+    count_calls()
+    return result
+
+def is_contains(string, list_to_search):
+    string = str(string).lower()
+    list_to_search = list(list_to_search)
+    count_calls()
+    for i in range(len(list_to_search)):
+        if str(list_to_search[i]).lower() == string:
+            result = True
+            break
+        else:
+            result = False
+            continue
+    return result
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+
+print(calls)
+
+
+
+
+
