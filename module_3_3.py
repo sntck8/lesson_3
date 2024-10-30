5438913cfe01e1b0dc0ff00ad6621b80768dc91d
sntck8 <sntck88@gmail.com>
2024-10-30 20:10:50 +0700

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,19 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+print_params(5, 'String')
+print_params( 7,None, False)
+print_params(666)
+print_params()
+
+values_list = [18, "sTrOkA", 111.222]
+values_dict = {"a": 2, "b": "Строчка", "c": False }
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 =[44, "Forty_four"]
+
+print_params(*values_list_2, 42)
+print("----------------------")
+print("Все параметры работают")
